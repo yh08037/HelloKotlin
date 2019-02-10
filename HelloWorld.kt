8a6569ab4eacfb9ffcb6ddca733c97d6dec68bc3
Dohun Kim <yh08037@gmail.com>
2019-02-10 21:15:24 +0900

    Create HelloWorld.kt

---
+++ HelloWorld.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Hello, World!")
+}
+/*  args: Array<String>
+     - 함수 인자를 표기할 때 "변수명: 타입" 순서로 쓴다
+     - 제네릭을 자바롸 동일하다
+     - Kotlin 1.3 부터 필수요소가 아니다
+    println()
+     - System.out.println() 을 간단하게 사용할 수 있다
+     - 이는 표준 자바 라이브러리 함수를 간소화해주는 wrapper 가 제공되기 때문이다
+     - 세미콜론(;)은 특수한 경우 외에는 선택사항이다
+ */
